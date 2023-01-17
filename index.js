943014428d67b1c602c610c1d0ae7a03229bfd02
Jared Krajewski <krajewski.jared@gmail.com>
2023-01-16 18:13:32 -0800

    + folder creation on upload, + delete function

--- index.js
+++ index.js
@@ -11,8 +11,16 @@ import {
   createSolidDataset,
   createThing,
   setThing,
+  // deleteSolidDataset, // for delete dataset
+  deleteFile,
+  createContainerAt, // for delete file
 } from "@inrupt/solid-client";
 import { Session } from "@inrupt/solid-client-authn-browser";
+// login,
+// handleIncomingRedirect, // for delete dataset
+// getDefaultSession, // for delete dataset
+// fetch, // for delete dataset
+
 import { rdf } from "@inrupt/solid-client/dist/constants";
 import { SCHEMA_INRUPT } from "@inrupt/vocab-common-rdf";
 
@@ -31,6 +39,7 @@ const session = new Session();
 const buttonLogin = document.getElementById("btnLogin");
 const writeForm = document.getElementById("writeForm");
 const queryForm = document.getElementById("queryData");
+const deleteForm = document.getElementById("deleteDocument");
 
 // 1a. Start Login Process. Call login() function from session class;
 // https://docs.inrupt.com/developer-tools/api/javascript/solid-client-authn-browser/classes/Session.html#login  ;
@@ -61,6 +70,7 @@ async function handleRedirectAfterLogin() {
     // populates fields for write and query by removing hidden attribute from divs;
     document.getElementById("write").removeAttribute("hidden");
     document.getElementById("query").removeAttribute("hidden");
+    document.getElementById("delete").removeAttribute("hidden");
     console.log(`session info: ${session.info.webId.split("profile")[0]}`);
   }
 }
@@ -85,13 +95,17 @@ async function handleFiles() {
   // logs the file added through input_file HTML form;
   console.log(fileList);
 
+  const folder = MY_POD_URL + documentValue;
+  await createContainerAt(folder, { fetch: session.fetch }).then((response) => {
+    console.log("hello", response);
+  });
+
   // assigning function to upload file, provide file from HTML input defined above and url where to upload;
   // function placeFileInContainer is defined below, so by the laws of javascript top down this confuses me but it works probably because its async;
   const uploadedFile = await placeFileInContainer(fileList, `${MY_POD_URL}`);
 
   // assigning url from where you want to call data. In this case folder as you need to check if ttl file already exists;
-  // this is declared in more than one place, not sure why just yet, doesn't seem like it needs function scope but I'll check that later;
-  const exampleSolidDatasetURL = session.info.webId.split("profile")[0];
+  const exampleSolidDatasetURL = folder;
 
   // https://docs.inrupt.com/developer-tools/api/javascript/solid-client/modules/resource_solidDataset.html#getsoliddataset  ;
   let checkDataSetForTTL = await getSolidDataset(
@@ -217,7 +231,7 @@ async function placeFileInContainer(file, targetContainerURL) {
 
 async function queryFiles() {
   // specify url from where you want to call data. In this case folder as you need to check if ttl file already exists;
-  // const exampleSolidDatasetURL = session.info.webId.split("profile")[0];
+  const exampleSolidDatasetURL = session.info.webId.split("profile")[0];
 
   let checkDataSetForTTL = await getSolidDataset(
     exampleSolidDatasetURL,
@@ -290,6 +304,33 @@ async function queryFiles() {
   }
 }
 
+async function deleteDocument() {
+  const documentDelete = document.getElementById("input_deleteDocument").value;
+  console.log(documentDelete);
+  const abc = await deleteFile(documentDelete, { fetch: session.fetch })
+    .then((response) => {
+      console.log("hello", response);
+    })
+    .catch((error) => {
+      console.log(error.message);
+    });
+}
+
+// test for file delete because pod browser is too slow.
+// try {
+//   // Delete the specified file from the Pod.
+//   await deleteFile(
+//     "https://example.com/some/boring/file", // File to delete
+//     { fetch: fetch } // fetch function from authenticated session
+//   );
+//   console.log("Deleted::  https://example.com/some/boring/file");
+// } catch (err) {
+//   console.error(err);
+// }
+
+// end of test for file delete
+
+// functions for buttons defined at start of program;
 buttonLogin.onclick = function () {
   console.log("logging in");
   login();
@@ -305,7 +346,126 @@ queryForm.addEventListener("submit", (event) => {
   queryFiles();
 });
 
+deleteForm.addEventListener("submit", (event) => {
+  event.preventDefault();
+  deleteDocument();
+});
+
 /////////// for sparql query test not done yet;
 let testQuery = document.getElementById("testQuery");
 let queryResult = testQuery.innerHTML;
+
+// let testQuery = document.getElementById("testQuery").innerHTML()
 ///////////
+
+// 1 // 1 Importing external libraries
+// 2 import { fetch } from 'solid−auth−client';
+// 3 const hellowWorldURL = 'https://janschill.solidcommunity.net/public/hello−world.ttl';
+// 4 const $rdf = require('rdflib');
+// 5
+// 6 // 2 Setting up local store
+// 7 const store = $rdf.graph();
+// 8 // 3 Patching browser fetch function
+// 9 const fetcher = new $rdf.Fetcher(store, {
+// 10 fetch: async (url, options) => {
+// 11 return await fetch(url, options)
+// 12 }
+// 13 });
+// 14 // 4 Creating linked resource
+// 15 const subject = store.sym(`${hellowWorldURL}#this`);
+// 16 const predicate = store.sym('https://example.org/message');
+// 17 const object = store.literal('Hello World');
+// 18 const document = subject.doc();
+// 19
+// 20 async function main(){
+// 21 // 5 Login using solid−auth−client
+// 22 // 6 Adding resource to local store
+// 23 store.add(subject, predicate, object, document);
+// 24 // 7 Loading updated local store to remote server
+// 25 await fetcher.putBack(document);
+// 26 }
+// 27 main();
+
+// 1 Import External Libraries The two libraries help with the intricate work of authentication with a Solid
+// data pod and all the needed work with RDF data.
+// 13
+// 2 Setting Up Local Store A local RDF store or graph is created and can be used to add, remove, or update
+// the resource in it. At the moment, it is not synchronized with any remote graph and is empty.
+// 3 Patch Browser Fetch Function The Fetcher is a helper to connect with a remote store. The local store
+// is passed in and its fetch method patched to use the one from the solid-auth-client, which allows
+// authenticated requests with a data pod.
+// 4 Creating a Linked Resource In this step, a LDPR is created. It uses the previously defined URI, which will
+// be later used to find the remote resource to update.
+// 5 Login Using solid-auth-client In this step a session needs to be established with the RS holding the to be
+// updated resource under the helloWorldURL. This part is omitted as it is a client-side flow, where at this
+// part the user would be prompted to authenticate with their AS. The session, which is persisted in a cookie
+// is automatically picked up by the fetch function from solid-auth-client.
+// 6 Adding Resource To Local Store This step adds the created resource to the local store.
+// 7 Updating Remote Store with Local Store The local store is now uploaded to the remote server and updates
+// the resource specified.
+
+// TEST PROFILE
+// https://testorg.solidcommunity.net/profile/card#me
+
+// LINKS FOR AUTH setup
+
+// https://docs.inrupt.com/developer-tools/api/javascript/solid-client-authn-browser/classes/Session.html#info
+// https://docs.inrupt.com/developer-tools/javascript/client-libraries/tutorial/authenticate-client/
+// https://github.com/linkeddata/rdflib.js/
+// https://github.com/inrupt/solid-client-authn-js/tree/8d57b6f4b22945550218655f6d838a01182fbfae
+// https://docs.inrupt.com/developer-tools/javascript/client-libraries/tutorial/read-write-data/   read write with auth links
+// https://docs.inrupt.com/ess/latest/services/service-pod-management/#pod-storage-resource-rdf
+// https://docs.inrupt.com/ess/latest/security/access-requests-grants/
+// https://docs.inrupt.com/ess/latest/security/identity-based-access/
+// https://docs.inrupt.com/ess/latest/security/acp/
+// https://github.com/solid-contrib/solid-node-client
+
+// # ACL resource for the profile Inbox
+
+// @prefix acl: <http://www.w3.org/ns/auth/acl#>.
+// @prefix foaf: <http://xmlns.com/foaf/0.1/>.
+
+// <#owner>
+//     a acl:Authorization;
+
+//     acl:agent
+//         <https://otman.solid.community/profile/card#me>;
+
+//     acl:accessTo <./>;
+//     acl:defaultForNew <./>;
+
+//     acl:mode
+//         acl:Read, acl:Write, acl:Control.
+
+// # Public-appendable but NOT public-readable
+// <#public>
+//     a acl:Authorization;
+
+//     acl:agentClass foaf:Agent;  # everyone
+
+//     acl:accessTo <./>;
+
+//     acl:mode acl:Append.
+
+// LINKS FOR FS
+// https://forum.solidproject.org/t/problem-with-retrieving-pieces-of-data-from-separate-files/5271
+// https://docs.inrupt.com/ess/latest/pod-resources/
+
+// CREATING A POD
+// https://docs.inrupt.com/ess/latest/services/service-pod-management/#pod-storage-resource-rdf
+
+//
+// apparently clearing browser cache can cause an error with loigin and "tokeType"
+// Session.ts:217 Uncaught (in promise) TypeError: Cannot read properties of undefined (reading 'tokenType')
+//     at Session.login (Session.ts:217:28)
+//     at login (defaultSession.ts:65:18)
+//     at buttonLogin.onclick (index.js:317:3)
+//
+
+// LOGINS
+
+// User: testorg, Password: @LBjLpFpiQf7aJK
+// URL https://testorg.solidcommunity.net/profile/card#me
+
+// passTest1  passPass1!
+//
